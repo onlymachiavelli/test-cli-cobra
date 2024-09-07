31cce27ec01b657f5153033609e6dbbc81d5f6a3
onlymachiavelli <tmakaveli643@gmail.com>
2024-09-07 22:51:27 +0100

    feat: :tada: init the command line interface

---
+++ go.mod
@@ -0,0 +1,3 @@
+module test-li
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+ 
+func init () {
+	fmt.Println("Hit the main file")	
+}
+
+
+func main () {
+	fmt.Println("Hello World")
+}
